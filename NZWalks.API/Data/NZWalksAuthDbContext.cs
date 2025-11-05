76b35f2e82e85960b1168f988f2c8890f70151b3
Eunsung Cho <eunsung.cho0901@gmail.com>
2025-11-06 00:28:40 +1300

    Authentication and Authorization 1

---
+++ NZWalks.API/Data/NZWalksAuthDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksAuthDbContext : IdentityDbContext
+    {
+        public NZWalksAuthDbContext(DbContextOptions<NZWalksAuthDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var readerRoleId = "d1e30f63-4833-4fd0-b31e-27ccd6038359";
+            var writerRoleId = "fe10012b-c1fa-4f5d-90b5-c06784eaed3c";
+
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = readerRoleId,
+                    ConcurrencyStamp = readerRoleId,
+                    Name = "Reader",
+                    NormalizedName = "Reader".ToUpper()
+                },
+                new IdentityRole {
+                    Id = writerRoleId,
+                    ConcurrencyStamp= writerRoleId,
+                    Name = "Writer",
+                    NormalizedName = "Writer".ToUpper()
+                }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
+    }
+}
